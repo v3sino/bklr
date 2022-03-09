77026bdb4692ae28b6cf50373450cdf3c4b0dd96
Andrej Žabka <72700765+v3sino@users.noreply.github.com>
2022-03-09 18:03:42 +0100

    Update formater3.js

--- examples/market/formater3.js
+++ examples/market/formater3.js
@@ -3,7 +3,7 @@ module.exports = {
         result = "";
         
         /* change this code to have nicely parsed output html data - put all the html into the result string */
-        result += "</div class=\"Crypto\">" + "<h2>Cryptocurrencies</h2>"; 
+        result += "<div class=\"Crypto\">" + "<h2>Cryptocurrencies</h2>"; 
         x = JSON.parse(data);
         result += "<table><tr><th>Crypto</th><th>Price</th><th>24H Change</th></tr>";
         for(let i=0; i < 3; i++){
